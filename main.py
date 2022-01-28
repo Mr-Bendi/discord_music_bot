5d20dabd2a4a373a18b0ded5da2170d26df6806d
BENDI <neubauer505@googlemail.com>
2022-01-28 16:40:36 +0100

    reworked the way we store the state of the bot, making multiple server use again possible

--- main.py
+++ main.py
@@ -20,8 +20,8 @@ if __name__ == "__main__":
     ccm = SpotifyClientCredentials(
         client_id=spotify_client_id, client_secret=spotify_client_secret
     )
-    spotify_instance = spotipy.Spotify(client_credentials_manager=ccm)
-    ytdl_options = {
+    SPOTIFY_INSTANCE = spotipy.Spotify(client_credentials_manager=ccm)
+    YTDL_OPTIONS = {
         "format": "bestaudio/best",
         "noplaylist": True,
         "forceurl": True,
@@ -34,7 +34,7 @@ if __name__ == "__main__":
         ],
     }
 
-    bot = musicbot.MusicBot(None, [], None, None,  False, ytdl_options, spotify_instance, command_prefix="-")
+    bot = musicbot.MusicBot({}, YTDL_OPTIONS, SPOTIFY_INSTANCE, command_prefix="-")
 
     @bot.event
     async def on_ready():
@@ -42,63 +42,65 @@ if __name__ == "__main__":
 
     @bot.command(aliases=["j"])
     async def join(ctx, bot=bot):
-        bot.voice_client = await ctx.message.author.voice.channel.connect(reconnect=True)
+        bot.state[ctx.message.author.voice.channel.id] = {"voice_client": None, "looping": None, "nowplaying": None, "lastplayed": None, "playlist": []}
+        bot.state[ctx.message.author.voice.channel.id]["voice_client"] = await ctx.message.author.voice.channel.connect(reconnect=True)
         print(f"Bot connected to voicechannel: \"{ctx.message.author.voice.channel.name}\"!")
         await ctx.message.add_reaction("👍")
 
     @bot.command(aliases=["bye"])
     async def leave(ctx, bot=bot):
-        await bot.voice_client.disconnect()
+        await bot.state[ctx.message.author.voice.channel.id]["voice_client"].disconnect()
         print("Bot disconnected!")
         await ctx.message.add_reaction("👍")
+        bot.state.pop(ctx.message.author.voice.channel.id)
 
     @bot.command(aliases=["pp"])
     async def pause(ctx, bot=bot):
-        bot.voice_client.pause()
+        bot.state[ctx.message.author.voice.channel.id]["voice_client"].pause()
         print("Music paused!")
         await ctx.message.add_reaction("👍")
         await ctx.channel.send("Paused!")
 
     @bot.command(aliases=["r"])
     async def resume(ctx, bot=bot):
-        bot.voice_client.resume()
+        bot.state[ctx.message.author.voice.channel.id]["voice_client"].resume()
         print("Music resumed!")
         await ctx.message.add_reaction("👍")
         await ctx.channel.send("Resumed!")
 
     @bot.command(aliases=["l"])
     async def loop(ctx, bot=bot):
-        if bot.looping:
+        if bot.state[ctx.message.author.voice.channel.id]["looping"]:
             await ctx.channel.send("No longer looping!")
             print("Looping turned on!")
         else:
             await ctx.channel.send("You are now looping!")
             print("Looping turned off!")
-        bot.looping = not bot.looping
+        bot.state[ctx.message.author.voice.channel.id]["looping"] = not bot.state[ctx.message.author.voice.channel.id]["looping"]
         await ctx.message.add_reaction("👍")
 
     @bot.command(aliases=["s"])
     async def skip(ctx, bot=bot, silent=False):
         #error for no reason
         try:
-            await bot.voice_client.stop()
+            await bot.state[ctx.message.author.voice.channel.id]["voice_client"].stop()
         except TypeError:
             pass
         if silent is False:
             print("Song skipped!")
             await ctx.channel.send("Skipped!")
             await ctx.message.add_reaction("👍")
-        playlist_handler(ctx)
+        playlist_handler(ctx.message.author.voice.channel.id)
 
     @bot.command(aliases=["np"])
     async def nowplaying(ctx, bot=bot):
         print("Sent current song!")
-        await ctx.channel.send(f"Now playing: {bot.nowplaying}")
+        await ctx.channel.send(f"Now playing: {bot.state[ctx.message.author.voice.channel.id]['nowplaying']}")
 
     @bot.command(aliases=["cp"])
     async def current_playlist(ctx, bot=bot):
-        if len(bot.playlist) != 0:
-            await ctx.channel.send(f"Current playlist: {bot.playlist}")
+        if len(bot.state[ctx.message.author.voice.channel.id]["playlist"]) != 0:
+            await ctx.channel.send(f"Current playlist: {bot.state[ctx.message.author.voice.channel.id]['playlist']}")
             print("Sent current playlist!")
         else:
             await ctx.channel.send("Playlist is empty!")
@@ -106,32 +108,32 @@ if __name__ == "__main__":
 
     @bot.command(aliases=["p"])
     async def play(ctx, *, song, bot=bot):
-        if not bot.voice_client.is_playing():
+        if not bot.state[ctx.message.author.voice.channel.id]["voice_client"].is_playing():
             try:
-                play_yt_song(song, bot.ytdl_options)
+                play_yt_song(song, bot.YTDL_OPTIONS, ctx.message.author.voice.channel.id)
                 await ctx.message.add_reaction("👍")
                 await nowplaying(ctx)
-                print(f"Now playing {bot.nowplaying}!")
+                print(f"Now playing {bot.state[ctx.message.author.voice.channel.id]['nowplaying']}!")
             except AgeRestrictedException:
                 await ctx.channel.send("This Video ist age restricted, sorry!")
                 print("Age restricted Video skipped!")
                 await skip(ctx, silent=True)    
         else:
-            bot.playlist.append(song)
+            bot.state[ctx.message.author.voice.channel.id]["playlist"].append(song)
             await ctx.channel.send("Song added!")
-            print(f"Added {song} to playlist!")
+            print(f"Added \"{song}\" to playlist!")
             await ctx.message.add_reaction("👍")
 
-    def play_yt_song(song, ytdl_options, bot=bot):
+    def play_yt_song(song, ytdl_options, voice_id, bot=bot):
         if "open.spotify.com" in song:
             print("Spotify Song detected!")
             song = spotify_link_handler.get_spotify_trackname(
-                song, bot.spotify_instance)
+                song, bot.SPOTIFY_INSTANCE)
         if not validators.url(song):
             if not song.startswith("ytsearch:"):
                 song = "ytsearch:" + song
 
-        with youtube_dl.YoutubeDL(bot.ytdl_options) as ydl:
+        with youtube_dl.YoutubeDL(bot.YTDL_OPTIONS) as ydl:
             try:
                 song_info = ydl.extract_info(song, download=False)
                 if not validators.url(song):
@@ -142,29 +144,28 @@ if __name__ == "__main__":
                     song_title = song_info["title"]
                 #dumb error
                 try:
-                    bot.voice_client.play(
+                    bot.state[voice_id]["voice_client"].play(
                         disnake.FFmpegPCMAudio(
                             song_url,
                             before_options="-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5",
-                        ),
-                        after=playlist_handler,
+                        )
                     )
-                except TypeError:
+                except ValueError:
                     pass
-                bot.lastplayed = song
-                bot.nowplaying = song_title
+                bot.state[voice_id]["lastplayed"] = song
+                bot.state[voice_id]["nowplaying"] = song_title
+                playlist_handler(voice_id)
             except youtube_dl.utils.DownloadError:
                 raise AgeRestrictedException()
 
         
 
-    def playlist_handler(error, bot=bot):
-
-        if not bot.looping:
-            if len(bot.playlist) != 0:
-                play_yt_song(bot.playlist.pop(0), bot.ytdl_options)
+    def playlist_handler(voice_id, bot=bot):
+        if not bot.state[voice_id]["looping"]:
+            if len(bot.state[voice_id]["playlist"]) != 0:
+                play_yt_song(bot.state[voice_id]["playlist"].pop(0), bot.YTDL_OPTIONS, voice_id)
         else:
-            play_yt_song(bot.lastplayed, bot.ytdl_options)
+            play_yt_song(bot.state[voice_id]["lastplayed"], bot.YTDL_OPTIONS, voice_id)
 
 
     #######
--- musicbot.py
+++ musicbot.py
@@ -2,14 +2,10 @@ import discord.ext.commands
 
 
 class MusicBot(discord.ext.commands.Bot):
-    def __init__(self, voice_client, playlist, nowplaying, lastplayed, looping, ytdl_options, spotify_instance, command_prefix="/", help_command=None, description=None):
+    def __init__(self, state, ytdl_options, spotify_instance, command_prefix="/", help_command=None, description=None):
         super().__init__(command_prefix, help_command, description)
-        self.voice_client = voice_client
-        self.playlist = playlist
-        self.nowplaying = nowplaying
-        self.lastplayed = lastplayed
-        self.looping = looping
-        self.ytdl_options = ytdl_options
-        self.spotify_instance = spotify_instance
+        self.state = state
+        self.YTDL_OPTIONS = ytdl_options
+        self.SPOTIFY_INSTANCE = spotify_instance
 
     
